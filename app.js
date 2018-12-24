058e2780577657da924112d3380bc6750904a261
Clayton Yocom <clayton@workwithgrid.com>
2018-12-23 21:05:10 -0500

    add scoring

--- app.js
+++ app.js
@@ -1,73 +1,5 @@
-var allTeams = [
-    "frc1024",
-    "frc1076",
-    "frc1189",
-    "frc1305",
-    "frc141",
-    "frc145",
-    "frc1468",
-    "frc1629",
-    "frc1739",
-    "frc1796",
-    "frc188",
-    "frc2013",
-    "frc2170",
-    "frc237",
-    "frc245",
-    "frc2512",
-    "frc27",
-    "frc2706",
-    "frc2708",
-    "frc2729",
-    "frc2767",
-    "frc2783",
-    "frc2877",
-    "frc3171",
-    "frc3175",
-    "frc3236",
-    "frc329",
-    "frc33",
-    "frc337",
-    "frc346",
-    "frc3534",
-    "frc3539",
-    "frc3637",
-    "frc3683",
-    "frc369",
-    "frc3950",
-    "frc401",
-    "frc4027",
-    "frc4308",
-    "frc4327",
-    "frc4362",
-    "frc4466",
-    "frc4481",
-    "frc4539",
-    "frc469",
-    "frc4811",
-    "frc4855",
-    "frc5030",
-    "frc5148",
-    "frc5243",
-    "frc5517",
-    "frc5561",
-    "frc5962",
-    "frc610",
-    "frc611",
-    "frc614",
-    "frc623",
-    "frc663",
-    "frc6753",
-    "frc6868",
-    "frc6964",
-    "frc7048",
-    "frc7153",
-    "frc74",
-    "frc747",
-    "frc810",
-    "frc836",
-    "frc888"
-];
+var allTeams = null;
+var remainingTeams = null;
 
 var entrants = [{
     name : 'BNI',
@@ -141,32 +73,52 @@ var entrants = [{
     score : null
 }];
 
-$(document).ready(function(){
-    allTeams = _.map(allTeams, function(t){
-        return Number(_.replace(t,'frc',''));
-    });
+var eventCode = null;
 
-    updateRemainingTeams();
+$(document).ready(function(){
     updatePicks();
-    startDraft();
 
     $('#pick_field').keypress(function(e) {
         if(e.which == 13) {
-            runDraft($('#pick_field').val());
+            if(eventCode != null){
+                runDraft($('#pick_field').val());
+            } else {
+                eventCode = $('#pick_field').val();
+                pollDataForEvent(eventCode);
+                updateRemainingTeams();
+
+                startDraft();
+            }
             $('#pick_field').val(null);
         }
     });
 });
+
+var pollDataForEvent = function pollDataForEvent(eventCode){
+    url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/keys";
+
+    $.ajaxSetup({
+        headers: { 'X-TBA-Auth-Key': window.TBA_KEY }
+    });
+
+    $.ajax({ 'url' : url, async: false }).done(function(data){
+        allTeams = data;
+        allTeams = _.map(allTeams, function(t){
+            return Number(_.replace(t,'frc',''));
+        });
+        allTeams.sort((a, b) => a - b);
+        remainingTeams = allTeams.slice();
+    });
+}
 
 var updateRemainingTeams = function updateRemainingTeams(){
     var template = $('#remaining_teams_template').text();
 
     compiled = _.template(template);
 
-    allTeams.sort((a, b) => a - b);
-
-    actualTeams = _.map(allTeams, function(t){
-        return getRealTeam(t);
+    actualTeams = _.map(remainingTeams, function(t){
+        retVal = getRealTeam(t);
+        return retVal;
     });
 
     $('.remaining_teams_list').html(compiled({teams : actualTeams}));
@@ -177,6 +129,8 @@ var updatePicks = function updatePicks(){
     compiled = _.template(template);
 
     entrants = _.sortBy(entrants,[function(e){
+        return e.score;
+    },function(e){
         return e.position;
     }]);
 
@@ -190,7 +144,11 @@ var updateBalloon = function(e){
 }
 
 var getRealTeam = function(team, allTeams){
-    return _.find(window.teamStrength, function(t){ return t.team == team});
+    ret = _.find(window.teamStrength, function(t){ return t.team == team});
+    if(!ret){
+        ret = { 'team' : team, elo : 1450 }
+    }
+    return ret;
 }
 
 var startDraft = function startDraft(){
@@ -213,13 +171,13 @@ var runDraft = function runDraft(t){
         return;
     }
 
-    if(!_.includes(allTeams, Number(t))){
+    if(!_.includes(remainingTeams, Number(t))){
         alert("team not available!");
         return;
     }
     teamPicked = getRealTeam(t);
     entrants[OTC].picks.push(teamPicked);
-    _.pull(allTeams, Number(t));
+    _.pull(remainingTeams, Number(t));
     if((OTC == entrants.length - 1 && round != 2) || (OTC == 0 && round == 2)){
         round++;
     } else if(round == 2){
@@ -233,5 +191,93 @@ var runDraft = function runDraft(t){
 }
 
 var computeScores = function computeScores(){
+    url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/district_points";
+    var robotPointsMap;
+    $.ajax({ 'url' : url, async: false }).done(function(data){
+        robotPointsMap = data.points;
+    });
 
+    url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/awards";
+    var awards;
+    $.ajax({ 'url' : url, async: false }).done(function(data){
+        awards = data;
+    });
+
+    entrants.forEach(function(entrant){
+        var score = 0;
+
+        entrant.picks.forEach(function(team){
+            //// just use TBA points dawg
+            // var innerTerm = ((numberOfTeams - (2 * rank.rank) + 2) / (alpha * numberOfTeams));
+            // console.log(innerTerm);
+            // var firstTerm = erfinv(innerTerm);
+            // console.log(firstTerm);
+            // var secondTerm = (10/erfinv(1/alpha));
+            // console.log(secondTerm);
+            // var result = (firstTerm * secondTerm) + 12;
+            // console.log(result);
+
+            tbaData = robotPointsMap["frc" + team.team];
+
+            robotPoints = tbaData.alliance_points;
+            robotPoints += tbaData.elim_points;
+            robotPoints += tbaData.qual_points;
+
+            score += robotPoints;
+
+            awardsForTeam = _.filter(awards,function(a){
+                return !!_.find(a.recipient_list,function(t){
+                    return Number(_.replace(t.team_key,'frc','')) == team.team;
+                });
+            });
+
+            var awardPoints = 0;
+
+            awardsForTeam.forEach(function(a){
+                points = _.find(window.awardPoints, function(ap){
+                    return ap.awardType == a.award_type;
+                });
+
+                awardPoints += points.points;
+            });
+
+            score += awardPoints;
+
+        });
+        entrant.score = score;
+    });
+
+
+
+    updatePicks();
+}
+
+
+
+//https://stackoverflow.com/a/12556710
+function erfinv(x){
+    var z;
+    var a  = 0.147;                                                   
+    var the_sign_of_x;
+    if(0==x) {
+        the_sign_of_x = 0;
+    } else if(x>0){
+        the_sign_of_x = 1;
+    } else {
+        the_sign_of_x = -1;
+    }
+
+    if(0 != x) {
+        var ln_1minus_x_sqrd = Math.log(1-x*x);
+        var ln_1minusxx_by_a = ln_1minus_x_sqrd / a;
+        var ln_1minusxx_by_2 = ln_1minus_x_sqrd / 2;
+        var ln_etc_by2_plus2 = ln_1minusxx_by_2 + (2/(Math.PI * a));
+        var first_sqrt = Math.sqrt((ln_etc_by2_plus2*ln_etc_by2_plus2)-ln_1minusxx_by_a);
+        var second_sqrt = Math.sqrt(first_sqrt - ln_etc_by2_plus2);
+        z = second_sqrt * the_sign_of_x;
+    } else { // x is zero
+        z = 0;
+    }
+
+    return z;
 }
